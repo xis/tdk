a44054ad189c5075cf5258def15ff30d19d92a59
xepea <fopeak@gmail.com>
2019-07-02 18:32:52 +0300

    tdk

--- tdk/tdk.py
+++ tdk/tdk.py
@@ -1,17 +1,22 @@
 import requests
 
-# returns word's meanings
-def search(word):
-    json = search_all_data(word)
-    meanings = []
-    for information in json[0]["anlamlarListe"]:
-        meanings.append(information["anlam"])
-    return meanings
+class new_word:
+    def __init__(self, word):
+        self.word = word
+        self.json = self.all_data()
 
-# returns word's json data
-def search_all_data(word):
-    r = requests.get('http://sozluk.gov.tr/gts', params={"ara":word})
-    json = r.json()
-    if 'error' in json:
-        raise Exception(json["error"])
-    return r.json()
+    # returns word's meanings
+    def meaning(self):
+        json = self.json
+        meanings = []
+        for information in json[0]["anlamlarListe"]:
+            meanings.append(information["anlam"])
+        return meanings
+
+    # returns word's json data
+    def all_data(self):
+        r = requests.get('http://sozluk.gov.tr/gts', params={"ara":self.word})
+        json = r.json()
+        if 'error' in json:
+            raise Exception(json["error"])
+        return json
