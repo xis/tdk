98f604a97e8819d6128d650125bb1543e4a7f62d
xepea <fopeak@gmail.com>
2019-07-02 18:28:59 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ setuptools.setup(
     version="1.0.0",
     author="Enes Furkan Olcay",
     author_email="dandikbirdalga@gmail.com",
-    description="tdk api yan sanayisi",
+    description="fetches meanings of the turkish words from sozluk.gov.tr",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xepea/tdk",
     packages=setuptools.find_packages(),
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=["requests"], 
 )
